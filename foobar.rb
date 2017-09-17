4602932aebb5f49662ce17f49e61b659b85a317a
Nicholas <nstruong13@berkeley.edu>
2017-09-17 13:33:06 -0700

    finished homework

--- foobar.rb
+++ foobar.rb
@@ -2,6 +2,14 @@ class Foobar
 
   def self.baz(a)
     # Class method
+    result = Array.new
+    for num in a
+      num = (num.to_i) + 2
+      if num % 2 == 0 and not result.include?(num) and num <= 10
+        result.push(num)
+      end
+    end
+    return result.sum()
     # Call with `Foobar.baz`
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,20 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a+b) * (a+b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+  for x in (0...a.length)
+     a[x] = a[x] + 1
+  end
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
